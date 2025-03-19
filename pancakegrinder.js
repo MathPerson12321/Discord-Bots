b0248e4c92972e73a1e7a8aed5613948f8acdde4
MathPerson12321 <mathguy12321@gmail.com>
2025-03-19 17:01:26 -0600

    Update pancakegrinder.js

--- pancakegrinder.js
+++ pancakegrinder.js
@@ -11,36 +11,9 @@
 
   async function sendHighlow() {
     while (true) {
-        await api.sendMessage(channelId, "p!highlow");
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await api.sendMessage(channelId, "lower");
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        sendDep();
-        await new Promise(resolve => setTimeout(resolve, 31000));
+        await api.sendMessage(channelId, "ok");
     }
-  }
-
-  async function sendWork() {
-      while (true) {
-          await api.sendMessage(channelId, "p!work");
-          await new Promise(resolve => setTimeout(resolve, 1000));
-          sendDep();
-          await new Promise(resolve => setTimeout(resolve, 350000));
-      }
-  }
-
-async function sendTrivia() {
-      while (true) {
-          await api.sendMessage(channelId, "p!trivia hard");
-          await new Promise(resolve => setTimeout(resolve, 1000));
-          await api.sendMessage(channelId, "2");
-          await new Promise(resolve => setTimeout(resolve, 1000));
-          sendDep();
-          await new Promise(resolve => setTimeout(resolve, 650000));
-      }
   }
   // Run both functions simultaneously
   sendHighlow();
-  sendWork();
-  sendTrivia();
 }
